e7c81f411f175f67e33d5f979b9cee00033c201e
Udayan Shiralkar <udayan75@hotmail.com>
2012-10-01 20:46:44 +0100

    lab complete

--- lab.rb
+++ lab.rb
@@ -2,9 +2,27 @@
 # declare hash representing the suite of a deck of cards, 
 # with abbreviations for the keys and full suit names for the values
 
-suites = {D: "Diamonds", H: "Hearts", C: "Clubs", S: "Spades"}
+suits = {D: "Diamonds", H: "Hearts", C: "Clubs", S: "Spades"}
 
 # for each rank (1 through 13) in each suit (C, D, H, S) display the rank name 
 # (e.g., "Ace", "Queen", "9" etc.) followed 
 # by its full suit name ("Hearts", "Spades", etc.).
+
+suits.each_value do |suit|
+
+	13.times do |num| 
+		rank = case num
+			when 0 then "ace"
+			when 1..9 then num + 1
+			when 10 then "jack"
+			when 11 then "queen"
+			else 
+				"king"
+		end		
+		puts "#{rank} of #{suit}"
+	end	
+	
+end
+	
+
 
